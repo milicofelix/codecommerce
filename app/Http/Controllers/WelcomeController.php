30a0bf27379a89ebaf2c9a0887346df2c3a51837
Adriano Bradbury <milicofelix@gmail.com>
2015-07-15 19:25:19 -0300

    Corrigindo alguns contra-tempos

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace CodeCommerce\Http\Controllers;
 
+use CodeCommerce\Category;
 use Illuminate\Http\Request;
 
 use CodeCommerce\Http\Requests;
@@ -9,11 +10,26 @@ use CodeCommerce\Http\Controllers\Controller;
 
 class WelcomeController extends Controller
 {
+    protected $category;
+    public function __construct(Category $category){
+        $this->category = $category;
+    }
+
     public function index(){
         $tests = array(
                         'frutas'=>array('Banana','Laranja','Uva','Pêra','Maçã'),
                         'legumes'=> array('Cenoura','Berinjela','Batatinha','Cebola')
                       );
         return view('welcome.index',compact('tests'));
+    }
+    public function example(){
+        $categories = $this->category->all();
+
+        return view('welcome.category',compact('categories'));
+    }
+    public function example2(){
+        $categories = $this->category->all();
+
+        return view('welcome.category_list',compact('categories'));
     }
 }
